743acc8284fbc96ddfd3216944cf55a3f461319d
Katherin_Z <zyrianovakate10@gmail.com>
2020-05-22 01:51:51 +0300

    store

---
+++ main/main.js
@@ -0,0 +1,8 @@
+let burg_menu = document.getElementById('burger_menu')
+
+document.getElementById('burger').onclick = function(){
+   burg_menu.style.display = 'block'
+ }
+document.getElementById('cross').onclick = function(){
+   burg_menu.style.display = 'none'
+ }
