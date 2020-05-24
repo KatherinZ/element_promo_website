1f88a4d308660e4c3b74b9723e757647f3bade3f
Katherin_Z <zyrianovakate10@gmail.com>
2020-05-24 13:51:48 +0300

    growth basics

--- main/main.js
+++ main/main.js
@@ -1,8 +1,17 @@
-let burg_menu = document.getElementById('burger_menu')
+let blocks = [...document.querySelectorAll('.block_1')].map(function(el) {
+  el.style.transform = `translate(${anime.random(5, 100)}vw, ${anime.random(0, 50)}vh)`
+})
+let randomItem = blocks[Math.floor(Math.random() * blocks.length)]
 
+
+document.getElementById('burger_menu')
 document.getElementById('burger').onclick = function(){
-   burg_menu.style.display = 'block'
+   document.getElementById('burger_menu').style.display = 'block'
  }
 document.getElementById('cross').onclick = function(){
-   burg_menu.style.display = 'none'
+   document.getElementById('burger_menu').style.display = 'none'
  }
+
+document.body.onclick = function(){
+  randomItem.style.display = 'block'
+}
---
+++ store/main.js
@@ -0,0 +1,7 @@
+document.getElementById('burger_menu')
+document.getElementById('burger').onclick = function(){
+   document.getElementById('burger_menu').style.display = 'block'
+ }
+document.getElementById('cross').onclick = function(){
+   document.getElementById('burger_menu').style.display = 'none'
+ }
